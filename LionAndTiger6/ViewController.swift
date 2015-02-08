8a5f65b6d39910d5f56ed7c96f817163fac5f381
delmarz <delmarz@delmarzs-Mac-Pro.local>
2015-02-08 16:32:47 +0800

    Initial Commit

---
+++ LionAndTiger6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LionAndTiger6
+//
+//  Created by delmarz on 2/8/15.
+//  Copyright (c) 2015 delmarz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
